d43060fb2efbf1059dc5419d23d2789c19f85124
Antonio Angelino <ilwebmastro@gmail.com>
2013-10-29 12:41:01 +0100

    Update utils.php
    Added comments

--- utils.php
+++ utils.php
@@ -4,12 +4,19 @@ require_once "lib/S3.php";
 
 S3::$useExceptions = true;
 
+/*
+ * Ritorna una istanza della Libreria S3 correttamente istanziata 
+ * per accedere al servizio di object storage di Aruba cloud  
+ */
 function getArubaStorage() {
 	global $config;
 	return new S3($config['accessKey'], $config['secretKey'], false, $config['accessPoint']);
 }
 
-
+/*
+ * La funzione controlla l'esistenza di un determinato file sul 
+ * bucket specificato nel file di configurazione 
+ */
 function checkFileExists($URI){
 	global $config;
 	$arubaStorage = getArubaStorage();
@@ -17,6 +24,10 @@ function checkFileExists($URI){
 
 }
 
+/*
+ * La funzione genera un URL pubblico per raggiungere un file
+ * archiviato sul proprio bucket
+ */
 function generatePath($URI){
 	global $config;
 	return "http://" . implode("/", array($config['accessPoint'], $config['bucketName'], $URI));
